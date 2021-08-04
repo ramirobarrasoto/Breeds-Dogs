5772978d364e697f338378e0eb2c51cc995a156f
ramirobarrasoto <rbarrasoto@gmail.com>
2021-08-04 12:34:38 -0300

    api

--- client/src/index.js
+++ client/src/index.js
@@ -10,7 +10,7 @@ import store from './Store/indexStore';
 import dotenv from "dotenv";
 dotenv.config()
 
-axios.defaults.baseURL=process.env.REACT_APP_API_KEY||"http://localhost:3001"
+axios.defaults.baseURL=process.env.REACT_APP_API||"http://localhost:3001"
 ReactDOM.render(
 	<React.StrictMode>
 		<Provider store={store}>
